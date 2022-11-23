6c5db6bd2b4b239395ef4e930d31fe1ae00eb0c2
OUTROS <118532389+miuka22@users.noreply.github.com>
2022-11-23 10:28:09 -0300

    g
    programar
    ...

---
+++ java.js
@@ -0,0 +1,10 @@
+
+function funcaoEmailAntonio() {
+    alert("Email do Antonio é: avps2@aluno.ifal.edu.br")
+}
+function funcaoEmailJere() {
+    alert("Email do Jeremias é: jvg3@aluno.ifal.edu.br")
+}
+function funcaoEmailJeall() {
+    alert("Email do Jeallyson é: jfs63@aluno.ifal.edu.br")
+}
